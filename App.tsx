68b30910c58ba056f82c674401ab62603dfeb4b7
stivel23 <s22110590@student.unklab.ac.id>
2025-09-10 10:29:28 +0800

    login

--- App.tsx
+++ App.tsx
@@ -1,118 +1,149 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
   View,
+  StyleSheet,
+  Image,
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const Exercise2 = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.title}>Biodata Mahasiswa</Text>
+      </View>
+      <View>
+        <Image
+          style={styles.profile}
+          source={require('./img/logounklab.jpg')}
+        />
+      </View>
+      <View>
+        <Text style={styles.line}>
+          ------------------------------------------------------------------------
+        </Text>
+        <Text style={styles.part1}>Nama: Waworuntu Stivel</Text>
+        <Text style={styles.part1}>TTL: Uuwan, 23 September 2003</Text>
+        <Text style={styles.part1}>Gender: Laki-Laki</Text>
+        <Text style={styles.part1}>Alamat: Dumoga, Uuwan</Text>
+        <Text style={styles.part1}>Email: S**@gmail.com</Text>
+        <Text style={styles.part1}>Kewarganegaraan: Indonesia</Text>
+        <Text style={styles.part1}>Agama: Kristen Protestan, GMIMB</Text>
+        <Text style={styles.part1}>Hobi: Memancing</Text>
+        <Text style={styles.part1}>Keahlian: Tetap terlihat tenang</Text>
+        <Text style={styles.line}>
+          ------------------------------------------------------------------------
+        </Text>
+        <Text style={styles.part2}>
+          Nama Kuliah: Universitas Klabat, Airmadidi
+        </Text>
+        <Text style={styles.part2}>
+          Fakultas: Fakultas Ilmu Komputer - Filkom
+        </Text>
+        <Text style={styles.part2}>Jurusan: Informatika</Text>
+        <Text style={styles.part2}>Semester: 8</Text>
+        <Text style={styles.part2}>Tingkat: 4</Text>
+        <Text style={styles.line}>
+          ------------------------------------------------------------------------
+        </Text>
+        <Text style={styles.part3}>
+          1. Apakah kita berkuliah di tempat yang sama?
+        </Text>
+        <TextInput style={styles.input} placeholder="Enter your answer" />
+        <Text style={styles.part3}>
+          2. Apakah kita Memiliki hobi yang sama?
+        </Text>
+        <TextInput style={styles.input} placeholder="Enter your answer" />
+        <Text style={styles.part3}>3. Apa keahlian anda?</Text>
+        <TextInput style={styles.input} placeholder="Enter your answer" />
+        <TouchableOpacity style={styles.button} activeOpacity={0.8}>
+          <Text style={styles.txt}>Submit</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.closebutton} activeOpacity={0.8}>
+          <Text style={styles.closetxt}>Close Profile</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
-}
+};
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+export default Exercise2;
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    backgroundColor: 'navy',
+    borderColor: 'black',
+    borderWidth: 5,
+    padding: 20,
+    margin: 20,
+    borderRadius: 100,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  title: {
+    fontSize: 50,
+    fontWeight: '800',
+    color: 'cyan',
+    textAlign: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  profile: {
+    height: 500,
+    width: 300,
+    margin: 20,
+    padding: 20,
   },
-  highlight: {
-    fontWeight: '700',
+  line: {
+    fontWeight: 'bold',
+  },
+  part1: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 10,
+    fontStyle: 'italic',
+    marginLeft: 10,
+  },
+  part2: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 10,
+    fontStyle: 'italic',
+    marginLeft: 10,
+  },
+  part3: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 10,
+    fontStyle: 'italic',
+    marginLeft: 10,
+  },
+  input: {
+    borderWidth: 3,
+    borderColor: 'black',
+    margin: 10,
+    fontSize: 20,
+    borderRadius: 10,
+  },
+  button: {
+    backgroundColor: 'green',
+    margin: 20,
+    padding: 20,
+    borderRadius: 50,
+  },
+  txt: {
+    color: 'white',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  closebutton: {
+    backgroundColor: 'red',
+    margin: 20,
+    padding: 20,
+    borderRadius: 50,
+  },
+  closetxt: {
+    color: 'white',
+    fontSize: 20,
+    textAlign: 'center',
   },
 });
-
-export default App;
